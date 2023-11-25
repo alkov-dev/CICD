b49a35d19cc190fc900db8a0f2e864ece8a6fdf9
magl88 <magl88net@gmail.com>
2023-11-25 15:44:52 +0100

    test 1

--- src/components/magl88/magl88.tsx
+++ src/components/magl88/magl88.tsx
@@ -3,7 +3,7 @@ import './magl88.css';
 export default function Magl88() {
   return (
     <div>
-      <h1>magl88</h1>
+      <h1>magl88 test</h1>
       <a href="https://github.com/magl88/">Magl88</a>
     </div>
   );
