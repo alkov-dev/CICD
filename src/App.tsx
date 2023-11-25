1e9f42e95c8ce3ed9cb50381e9e0d190472240bc
l4vrin <l4vrin@gmail.com>
2023-11-25 17:14:25 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Footer from './components/footer';
 import Magl88 from './components/magl88';
 import { Raijin } from './components/raijin/raijin';
+import Rn from './components/rn/rn';
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <p>App</p>
       <Raijin />
       <Magl88 />
+      <Rn />
       {/* <Footer /> */}
     </main>
   );
---
+++ src/components/rn/rn.tsx
@@ -0,0 +1,9 @@
+import "./rn.css";
+
+export default function Rn() {
+  return (
+    <div>
+      <a href="https://github.com/L4vrin" className='rn-link'>Rn</a>
+    </div>
+  );
+}
