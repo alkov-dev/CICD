df3e68d025711a7b9c4a2d5aac5ae38cf89d3c6a
alkovdev <darkzoner31@gmail.com>
2023-11-25 16:25:21 +0300

    new

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Footer from "./components/footer";
+import Magl88 from "./components/magl88";
 
 export default function App() {
   return (
     <main>
-      <p>App!!</p>
-      <Footer />
+      <p>App</p>
+      <Magl88 />
+      {/* <Footer /> */}
     </main>
   );
 }
---
+++ src/components/magl88.tsx
@@ -0,0 +1,9 @@
+import "./magl88.css";
+
+export default function Magl88() {
+  return (
+    <div>
+      <a href="#">Magl88</a>
+    </div>
+  );
+}
