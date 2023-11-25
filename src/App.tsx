9b3f70a11d2347b0c09a20246790353197be1697
Dinara9595 <dinara.petuhova95@yandex.ru>
2023-11-25 20:53:52 +0600

    Component dinara

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Footer from './components/footer';
 import Magl88 from './components/magl88/magl88';
 import { Raijin } from './components/raijin/raijin';
 import Rn from './components/rn/rn';
+import Dinara from "./components/dinara/dinara";
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <p>App</p>
       <Raijin />
       <Magl88 />
+      <Dinara />
       <Rn />
       {/* <Footer /> */}
     </main>
---
+++ src/components/dinara/dinara.tsx
@@ -0,0 +1,9 @@
+import "./dinara.css";
+
+export default function Dinara() {
+  return (
+    <div>
+      <a href="https://github.com/dinara8" className='dinara'>Dinara</a>
+    </div>
+  );
+}
