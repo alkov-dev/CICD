34722dfa62a19387878bc27da4f1710ffe7cad94
alkov-dev <152009103+alkov-dev@users.noreply.github.com>
2023-11-25 18:11:14 +0300

    Revert "Dinara"

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import Footer from './components/footer';
 import Magl88 from './components/magl88/magl88';
 import { Raijin } from './components/raijin/raijin';
 import Rn from './components/rn/rn';
-import Dinara from "./components/dinara/dinara";
 
 export default function App() {
   return (
@@ -10,7 +9,6 @@ export default function App() {
       <p>App</p>
       <Raijin />
       <Magl88 />
-      <Dinara />
       <Rn />
       {/* <Footer /> */}
     </main>
--- src/components/dinara/dinara.tsx
+++
@@ -1,9 +0,0 @@
-import "./dinara.css";
-
-export default function Dinara() {
-  return (
-    <div>
-      <a href="https://github.com/dinara8" className='dinara'>Dinara</a>
-    </div>
-  );
-}
